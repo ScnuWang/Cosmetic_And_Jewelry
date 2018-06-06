9be54cdcad6d4523a5a263e87ce4cae2e2c4fcc4
ScnuWang <scnu_wang@163.com>
2018-06-06 18:14:59 +0800

    添加数据库表模型结构类

---
+++ models/Cj_Model.py
@@ -0,0 +1,61 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DECIMAL, create_engine, DateTime
+
+Base = declarative_base()
+
+# 产品表
+class Cj_Product(Base):
+    __tablename__ = 'cj_product'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    original_id = Column('original_id', String, comment="原始编号")
+    brand_id = Column('brand_id', Integer, comment="品牌编号")
+    location_id = Column('location_id', Integer, comment="销售地点编号")
+    product_name = Column('product_name', String(255), comment="产品名称")
+    product_price = Column('product_price', DECIMAL(16, 2), comment="产品名称")
+    original_currency = Column('original_currency', String(255), comment="原始币种")
+    product_url = Column('product_url', String(255), comment="产品详情地址")
+    product_image = Column('product_image', String(512), comment="产品图片地址")
+    product_thumbnail = Column('product_thumbnail', String(512), comment="产品缩略图地址")
+    old_price = Column('old_price', DECIMAL(16, 2), comment="原始价格")
+    product_name_zh = Column('product_name_zh', String(512), comment="中文别名")
+    cny_product_price = Column('cny_product_price', DECIMAL(16, 2), comment="产品人民币价格")
+    cny_exchange_rate = Column('cny_exchange_rate', DECIMAL(16, 2), comment="对人民币汇率")
+    product_status_id = Column('product_status_id', Integer, comment="产品状态")
+    category_id = Column('category_id', Integer, comment="产品分类")
+    update_time = Column('update_time', DateTime, comment='更新时间')
+
+# 品牌表
+class Cj_Brand(Base):
+    __tablename__ = 'cj_brand'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    brand_name = Column('brand_name', String(255), comment="品牌名称")
+    update_time = Column('update_time', DateTime, comment='更新时间')
+
+# 分类表
+class Cj_Category(Base):
+    __tablename__ = 'cj_category'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    category_name_zh = Column('category_name_zh', String(255), comment="中文名称")
+    category_name_en = Column('category_name_en', String(255), comment="英文名称")
+    update_time = Column('update_time', DateTime, comment='更新时间')
+
+# 汇率表
+class Cj_Currency(Base):
+    __tablename__ = 'cj_currency'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    currency = Column('currency', String(255), comment="币种简写")
+    exchange_rate_cny = Column('exchange_rate_cny', String(255), comment="对换人民币汇率")
+    update_time = Column('update_time', DateTime, comment='更新时间')
+
+# 销售地点表
+class Cj_Location(Base):
+    __tablename__ = 'cj_location'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    sales_location = Column('sales_location', String(255), comment="销售地点")
+    update_time = Column('update_time', DateTime, comment='更新时间')
+
+class Cj_Product_Status(Base):
+    __tablename__ = 'cj_location'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    product_status = Column('product_status', String(255), comment="销售地点")
+    update_time = Column('update_time', DateTime, comment='更新时间')
--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 from sqlalchemy import Column,Integer,String,DECIMAL,create_engine,DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+from models import Cj_Model
 
 Base = declarative_base()
 
 
-class Product(Base):
-    __tablename__ = 'cj_product_test'
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    original_id = Column(String(255))
-    product_name = Column(String(255))
-    def __repr__(self):
-        return "<Product(id='%d', original_id='%s', product_name='%s')>" % (self.id, self.original_id, self.product_name)
 
 # 插入中文会报错，需要在数据库名称后面添加?charset=utf8
 engine = create_engine("mysql+pymysql://root:admin@localhost/data?charset=utf8", echo=True)
-Product.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
-product = Product(original_id='432143',product_name='施华洛世奇')
+product = Cj_Model.Cj_Brand(brand_name='Swarosvki',update_time=datetime.now())
 session = DBSession()
 session.add(product)
 session.commit()
